f8a50792a8481ed84f241ce0d2a1c2fc6932c4e8
IvSDF <demchyshyn.ivan@gmail.com>
2023-07-17 21:24:19 +0300

    add_project

---
+++ app/Http/Controllers/Api/V1/Admin/TourController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\TourRequest;
+use App\Http\Resources\TourResource;
+use App\Models\Travel;
+
+
+class TourController extends Controller
+{
+    public function store(Travel $travel, TourRequest $request)
+    {
+        dd($request);
+//        $tour->create([
+//            'travel_id' => '99a87ecd-bd0c-4bb0-9a1d-5793e895ac30',
+//            'name' => 'New tour',
+//            'starting_date' => '2023-07-17',
+//            'ending_date' => '2023-07-19',
+//            'price' => '190',
+//        ]);
+
+        $tour = $travel->tours()->create($request->validated());
+
+        return new TourResource($tour);
+
+//        dd($tour);
+
+//        return new TourResource($tour);
+    }
+}
+
---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Role;
+use App\Models\Travel;
+use Illuminate\Database\Seeder;
+use function Sodium\add;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+//        // \App\Models\User::factory(10)->create();
+//        \App\Models\Travel::factory(5)->create();
+//        $travels = Travel::all();
+////        dd($travel);
+//        foreach ($travels as $travel){
+//            \App\Models\Tour::factory(15)->create(
+//                [
+//                    'travel_id' => $travel,
+//                ]
+//            );
+//        }
+//
+        $this->call(RoleSeeder::class);
+
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
